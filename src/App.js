2315538840f07b0586efd6b760b1b847e309cc75
Lê Vũ Hoàng Khanh <lvhkhanh@gmail.com>
2019-02-19 21:12:13 +0700

    add angular link

--- src/App.js
+++ src/App.js
@@ -21,6 +21,23 @@ class App extends Component {
           </a> */}
           <p>Lê Vũ Hoàng Khanh</p>
           <p>Dev</p>
+          {/* <p>
+            <a href="https://cv-react.stackblitz.io">
+              <img
+                src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+                height="72"
+                alt="React"
+              />
+            </a>
+          </p> */}
+          <p>
+            <a href="https://angular-cv.stackblitz.io">
+              <img
+                src="https://angular.io/assets/images/logos/angular/logo-nav@2x.png"
+                alt="Angular"
+              />
+            </a>
+          </p>
         </header>
       </div>
     );
